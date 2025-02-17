56d067c4793eec5cb79a4d45e95c38b5d475808a
Josue <geenujunior@gmail.com>
2025-02-17 19:55:06 -0300

    adicionada checagem de colunas, criação de dataframe e obtenção de código de estado

--- functions.py
+++ functions.py
@@ -1,6 +1,69 @@
+from json import JSONDecodeError
+
 import pandas as pd
 import json
 import os
+
+
+# Constante com as variáveis representadas em cada coluna
+ANALYSIS_VARIABLES = ['DT_NOTIFIC', 'NU_ANO', 'ID_MUNICIP', 'SG_UF',
+                      'ANO_NASC', 'CS_SEXO', 'CS_GESTANT', 'CLASSI_FIN',
+                      'EVOLUCAO', 'DT_OBITO']
+
+
+# Verifica se todas as colunas necessárias estão no dataset
+def column_check(data_frame: str) -> bool:
+
+    df = pd.read_csv(data_frame)
+
+    for variable in ANALYSIS_VARIABLES:
+        if variable not in df.columns:
+            return False
+    return True
+
+
+def get_code_state(state_analysis: str) -> int:
+
+    try:
+        with open('src/code_state.json', 'r', encoding="utf-8") as f:
+            states = json.load(f)
+            code_found = None
+            for state in states:
+                if state[1] == state_analysis:
+                    code_found = int(state[-1])
+                    return code_found
+            return code_found
+
+    except FileNotFoundError:
+        print('Arquivo não encontrado, verifique a pasta rsc')
+    except JSONDecodeError:
+        print('Erro ao abrir arquivo JSON')
+
+a = get_code_state('PB')
+print(type(a))
+
+
+
+# Função que cria e retorna o dataframe formatado com as colunas necessárias
+def create_a_dataframe(state: str, file_path: str) -> pd.DataFrame:
+
+
+    if not column_check(file_path):
+        raise 'NAO TEM A COLUNA'
+
+    # pandas já informa se um das constantes não for encontrada como coluna
+    unformatted_data = pd.read_csv(file_path, sep=',', usecols=ANALYSIS_VARIABLES)
+
+    # cria um novo objeto por copia profunda
+    formatted_data = unformatted_data.copy(deep=True)
+
+    return formatted_data
+
+
+r = create_a_dataframe('PB', 'data/processed/LEIVBR14.csv')
+print(column_check('data/processed/LEIVBR14.csv'))
+print(r)
+
 
 def year_of_analysis(data: str) -> int:
     """
@@ -23,7 +86,7 @@ def convert_cnv_to_json(file: str, file_name_save: str) -> None:
     :param file_name_save: nome do arquivo final json
     """
 
-    BASE_DIR = os.path.dirname(os.path.abspath(file))
+    BASE_DIR = os.path.abspath("src")
     SAVE_TO = os.path.join(BASE_DIR, (file_name_save+".json"))
 
     # Sempre fazer um with open para leitura e outro para escrita?
@@ -34,10 +97,12 @@ def convert_cnv_to_json(file: str, file_name_save: str) -> None:
         for l in range(len(lines)):
             lines[l] = lines[l].split()
 
-        print(lines)
+        # Remove informações irrelevantes
+        lines.pop(0)
+        lines.pop(0)
+
 
         with open(SAVE_TO, 'w+', encoding="utf8") as a:
             json.dump(lines, a, indent=4, ensure_ascii=False)
 
-
-
+convert_cnv_to_json('data/processed/UFccodig.cnv', 'code_state')
