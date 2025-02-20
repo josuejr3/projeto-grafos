fb1184bf5d022e1079f5a11d140d8dc1fa8390c3
Josue <geenujunior@gmail.com>
2025-02-20 00:56:16 -0300

    separação de vértices de ano e cidades, definição aplicação de arestas sem peso e plotagem do grafo

--- functions.py
+++ functions.py
@@ -3,18 +3,28 @@ from json import JSONDecodeError
 import pandas as pd
 import json
 import os
-
-from pandas.core.interchange.dataframe_protocol import DataFrame
+import chardet
 
 # Constante com as variáveis representadas em cada coluna
 ANALYSIS_VARIABLES = ['DT_NOTIFIC', 'NU_ANO', 'ID_MUNICIP', 'SG_UF_NOT',
                       'ANO_NASC', 'CS_SEXO', 'CS_GESTANT', 'CLASSI_FIN',
                       'EVOLUCAO', 'DT_OBITO']
 
+YEARS = ['2014', '2015', '2016', '2017', '2018', '2019', '2020',
+        '2021', '2022', '2023', '2024']
 
-def city_name(list_names: list) -> list:
+
+# Função para identificação de tipo de codificação do arquivo
+def detected_encoding(arquivo):
+    with open(arquivo, "rb") as f:
+        resultado = chardet.detect(f.read(10000))  # Lê os primeiros 10KB para análise
+    return resultado["encoding"]
+
+
+#Obtém uma lista com código - nome
+def local_names(list_names: list) -> list:
     """
-    Função que retorna uma lista com o nome de o código
+    Função auxiliar para formatação do nome de cidades e estados que retorna o nome formatado e o codigo
     :param list_names: Lista com os dados da cidade/estado
     :return: Lista que retorna código e nome
     """
@@ -32,8 +42,8 @@ def column_check(data_frame: str) -> bool:
     :param data_frame: o dataframe usado para verificação
     :return: um boolean informando se todas as colunas estão presentes ou não.
     """
-
-    df = pd.read_csv(data_frame)
+    # low_memory = false
+    df = pd.read_csv(data_frame, low_memory=False)
 
     for variable in ANALYSIS_VARIABLES:
         if variable not in df.columns:
@@ -51,7 +61,6 @@ def get_code(value: str, file: str) -> None | str:
     value = value.upper() if len(value) == 2 else value.title()
 
     try:
-
         with open(file, 'r', encoding="utf-8") as f:
             dictionary_codes = json.load(f)
             if value in dictionary_codes.keys():
@@ -73,9 +82,10 @@ def create_a_dataframe(state: str, file_path: str) -> pd.DataFrame:
     :return: dataframe com as colunas necessárias para a análise e do estado correspondente
     """
 
-    # Obtém o código do estado
+    # Obtém o código do estado em formato de inteiro
     state = get_code(state, "src/states.json")
 
+    # Verifica se todas as colunas necessárias estão presentes
     if not column_check(file_path):
         raise "Uma das colunas necessárias não está presente no dataset"
 
@@ -84,12 +94,13 @@ def create_a_dataframe(state: str, file_path: str) -> pd.DataFrame:
 
     # cria um novo objeto por copia profunda
     formatted_data = unformatted_data.copy(deep=True)
+    # Seleciona somente as linhas em que a coluna "SG_UF_NOT" são do código referente ao estado escolhido
     formatted_data = formatted_data.loc[formatted_data['SG_UF_NOT'] == state]
-    return formatted_data
+    return formatted_data.fillna('X')
 
 
 
-def year_of_analysis(data: DataFrame) -> int:
+def year_of_analysis(data: pd.DataFrame) -> int:
     """
     Uma função para identificar qual o ano o arquivo está se referindo
     :param data: caminho do arquivo csv ou o próprio nome do arquivo com extensão
@@ -107,41 +118,36 @@ def convert_cnv_to_json(file: str, file_name_save: str) -> True:
     :param file_name_save: nome do arquivo final json
     """
 
+    # caminho absoluto da pasta src
     BASE_DIR = os.path.abspath("src")
+    # caminho onde o arquivo sera salvo
     SAVE_TO = os.path.join(BASE_DIR, (file_name_save+".json"))
 
     # dicionario que com cidade/estado que será transformado em json
     save_local = dict()
 
-    # Sempre fazer um with open para leitura e outro para escrita?
-    # ajustar o encoding, para estados utf8 para cidade iso
-
     try:
-        with open(file, 'r+') as f:
-            lines = f.readlines()
+        with open(file, 'r+', encoding=detected_encoding(file)) as f:
 
+            lines = f.readlines()
             for l in range(len(lines)):
                 lines[l] = lines[l].split()
 
-                dados_cidade = city_name(lines[l])
-                save_local[dados_cidade[1]] = dados_cidade[0]
+                # obtem as informações do local, codigo - nome
+                info_local = local_names(lines[l])
+                save_local[info_local[1]] = info_local[0]
 
-            # Remove informações irrelevantes
+            # Remove informações de cabeçalho
             lines.pop(0)
             lines.pop(0)
 
+            # Cria e escreve no arquivo json o dicionario com todas os estados/municipios
             with open(SAVE_TO, 'w+', encoding="utf8") as a:
                 json.dump(save_local, a, indent=4, ensure_ascii=False)
             return True
+
     except FileNotFoundError:
-        raise print('Arquivos base para cidades e munícipios não foram encontrados.')
+        raise print('Arquivos ou pastas base para cidades e munícipios não foram encontrados.')
     except JSONDecodeError:
         raise print('Erro de leitura no arquivo JSON dos arquivos de cidades e estados.')
 
-# convert_cnv_to_json('data/processed/Municpb.cnv', 'cities')
-# convert_cnv_to_json('data/processed/UFccodig.cnv', 'code_state')
-
-# print(get_code('campina grande', 'src/cities.json'))
-
-# df = create_a_dataframe('PB', 'data/processed/LEIVBR14.csv')
-# print(df)
--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+from numpy.testing.print_coercion_tables import print_coercion_table
+from pyparsing import withAttribute
+
 import functions
-from functions import convert_cnv_to_json, create_a_dataframe, year_of_analysis
+from functions import convert_cnv_to_json, create_a_dataframe, year_of_analysis, YEARS, detected_encoding
+import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
+import json
+
+# config para ver todas as colunas
+pd.options.display.width = None
+pd.options.display.max_columns = None
+pd.set_option('display.max_rows', 3000)
+pd.set_option('display.max_columns', 3000)
 
 if __name__ == '__main__':
 
@@ -11,4 +24,61 @@ if __name__ == '__main__':
     if not convert_uf and convert_cities:
         raise print('Não foi possível criar os arquivos base')
 
-    # Parte de grafos
+    entrada = 'PB'
+
+    my_graph = nx.DiGraph()
+    my_graph.add_nodes_from(YEARS)
+
+
+    for y in YEARS:
+
+        ultimos_digitos = y[2::]
+
+        cases_paraiba = create_a_dataframe(entrada, f'data/processed/LEIVBR{ultimos_digitos}.csv')
+
+        cities_list = set()
+
+        for case in cases_paraiba.iterrows():
+            # Serie contendo todas as informações do caso
+            case_data = case[1]
+            city_id = str(case_data['ID_MUNICIP'])
+            if city_id not in cities_list:
+                cities_list.add(city_id)
+
+
+        DIR_CITIES = "src/cities.json"
+
+        # Altera codigos por nomes das cidades
+        with open(DIR_CITIES, "r", encoding=detected_encoding(DIR_CITIES)) as f:
+            cities = json.load(f)
+
+            # Substitui os codigos pelo nome da cidade
+            for k, v in cities.items():
+                if v in cities_list:
+                    cities_list.add(k)
+                    cities_list.remove(v)
+
+        # cria da cidade e cria uma aresta que interliga a cidade ao ano
+        for city in cities_list:
+            my_graph.add_node(city)
+            my_graph.add_edge(city, str(year_of_analysis(cases_paraiba)))
+
+
+    # for node, degre in my_graph.degree():
+    #     print(f"O vertice: {node} tem grau {degre}")
+
+    color_nodes = []
+    size_nodes = []
+
+    for n in my_graph.nodes():
+        color_nodes.append('orange') if n in YEARS else color_nodes.append('red')
+
+    for n in my_graph.nodes():
+        size_nodes.append(3500) if n in YEARS else size_nodes.append(1500)
+
+
+    plt.figure(figsize=(40, 40))
+    nx.draw_networkx(my_graph, with_labels=True, arrows=True, node_color=color_nodes, node_size=size_nodes)# pos=nx.spring_layout(my_graph, seed=150))
+    plt.title('GRAFO LEISHMANIOSE NA PARAÍBA 2014-2024', size=75)
+    plt.show()
+
