ae571b94034b05dd9146bf3330298871f6690bd5
Josue <geenujunior@gmail.com>
2025-03-13 19:45:41 -0300

    refatoração de algumas funções e implementação de graficos de barras e setores

--- main.py
+++ main.py
@@ -2,60 +2,60 @@ from matplotlib.pyplot import title
 from networkx.algorithms.centrality import eigenvector_centrality
 from networkx.drawing import bipartite_layout
 
-from functions import (convert_cnv_to_json, create_a_dataframe, year_of_analysis, YEARS, detected_encoding,
-                       get_code, ranking, absance_of_cases_in_year, plot_graph, plot_subgraph, weight_of_city)
+from utils import (convert_cnv_to_json, create_a_dataframe, year_of_analysis, YEARS, detected_encoding,
+                   get_code, ranking, absance_of_cases_in_year, plot_graph, config_plot_subgraph, weight_of_city,
+                   show_ranking, cardinality, MESORREGIOES, DIR_CITIES, SERTAO, MATA, BORBOREMA, AGRESTE,
+                   MUNICIPIO_ID, COR_AGRESTE, COR_BORBOREMA, COR_MATA, COR_SERTAO, COR_ANO,
+                   plot_geral_graph, ESTADO, DIR_UFCODIG, DIR_MUNICIP, DIR_DATA_PROCESSED, CONSTANTE_MULT,
+                   CONSTANTE_SOMA, bar_chart, pie_chart)
 import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
 import json
 
+
 if __name__ == '__main__':
 
-    # Pré processamento - ok
-    convert_uf = convert_cnv_to_json("data/processed/UFccodig.cnv", "states")
-    convert_cities = convert_cnv_to_json("data/processed/Municpb.cnv", "cities")
+    # Pré processamento - convertendo arquivos cnv para JSON
+    convert_uf = convert_cnv_to_json(DIR_UFCODIG, "states")
+    convert_cities = convert_cnv_to_json(DIR_MUNICIP, "cities")
 
     # Se os arquivos JSON foram criados corretamente, crie o data frame
     if not convert_uf and convert_cities:
         raise print('Não foi possível criar os arquivos base')
 
-    input_state = 'PB'
-
+    # Criando subgrafos das subregiões e adicionando a faixa de anos em cada um
     sertao = nx.Graph()
     agreste = nx.Graph()
     mata = nx.Graph()
     borborema = nx.Graph()
 
-    sertao.add_nodes_from(YEARS)
-    agreste.add_nodes_from(YEARS)
-    mata.add_nodes_from(YEARS)
-    borborema.add_nodes_from(YEARS)
+    for regiao in [sertao, agreste, mata, borborema]:
+        regiao.add_nodes_from(YEARS)
 
-    for y in YEARS:
+    for year in YEARS:
 
-        ultimos_digitos = y[2::]
-        cases_paraiba = create_a_dataframe(input_state, f'data/processed/LEIVBR{ultimos_digitos}.csv')
+        last_digits_of_year = year[2::]
+        cases_paraiba = create_a_dataframe(ESTADO, f'{DIR_DATA_PROCESSED}{last_digits_of_year}.csv')
         cities_set = set()
+        dictionary_cities_with_codes = {}
 
-        dictionary_cities = {}
-
-        # Adiciona o código das cidades que tiveram casos no ano
+        # Adiciona ao dicionario de cidades com codigos as cidades com casos notificados
         for case in cases_paraiba.iterrows():
             # Set contendo todas as informações do caso
-            case_data = case[1]
-            city_id = str(case_data['ID_MUNICIP'])
+            infos_case = case[1]
+            # problema com o ano de 2023
+            city_id = str(int(infos_case[MUNICIPIO_ID]))
 
             if city_id not in cities_set:
                 cities_set.add(city_id)
-                dictionary_cities.update({city_id: {"year": y, "cases": 0}})
+                dictionary_cities_with_codes.update({city_id: {"year": year, "cases": 0}})
 
-        # Conta quantidade de casos
-        for c in dictionary_cities.keys():
-            cases_in_city = cases_paraiba['ID_MUNICIP'].value_counts().get(int(c), 0)
+        # Conta quantidade de casos em cada uma das cidades
+        for city_with_code_dict in dictionary_cities_with_codes.keys():
+            cases_in_city = cases_paraiba[MUNICIPIO_ID].value_counts().get(int(city_with_code_dict), 0)
 
-        DIR_CITIES = "src/cities.json"
-
-        # Altera codigos por nomes das cidades
+        # Altera codigos por nomes no set de cidades
         with open(DIR_CITIES, "r", encoding=detected_encoding(DIR_CITIES)) as f:
             cities = json.load(f)
 
@@ -65,140 +65,135 @@ if __name__ == '__main__':
                     cities_set.add(k)
                     cities_set.remove(v)
 
-        # cria da cidade e cria uma aresta que interliga a cidade ao ano[
-        with open('src/mesoregioes.json', 'r', encoding=detected_encoding(DIR_CITIES)) as f:
+        # Abre o arquivo de mesorregiões interliga o vértice da cidade com o ano de análise e adiciona ao subgrafo da
+        # região específica
+        with open(MESORREGIOES, 'r', encoding=detected_encoding(MESORREGIOES)) as f:
 
             regioes = json.load(f)
             for city in cities_set:
 
+                # Obtém o código da cidade e a quantidade de casos notificados nela ao longo do ano
                 code_of_city = get_code(city, DIR_CITIES)
-                weight_c = weight_of_city(code_of_city, cases_paraiba)
+                weight_of_edge_city = weight_of_city(code_of_city, cases_paraiba)
 
-                if city in regioes['SERTAO']:
+                if city in regioes[SERTAO]:
                     sertao.add_node(city)
-                    sertao.add_edge(city, year_of_analysis(cases_paraiba), weight=weight_c)
-                elif city in regioes['AGRESTE']:
+                    sertao.add_edge(city, year_of_analysis(cases_paraiba), weight=weight_of_edge_city)
+                elif city in regioes[AGRESTE]:
                     agreste.add_node(city)
-                    agreste.add_edge(city, year_of_analysis(cases_paraiba), weight=weight_c)
-                elif city in regioes['MATA']:
+                    agreste.add_edge(city, year_of_analysis(cases_paraiba), weight=weight_of_edge_city)
+                elif city in regioes[MATA]:
                     mata.add_node(city)
-                    mata.add_edge(city, year_of_analysis(cases_paraiba), weight=weight_c)
-                elif city in regioes['BORBOREMA']:
+                    mata.add_edge(city, year_of_analysis(cases_paraiba), weight=weight_of_edge_city)
+                elif city in regioes[BORBOREMA]:
                     borborema.add_node(city)
-                    borborema.add_edge(city, year_of_analysis(cases_paraiba), weight=weight_c)
+                    borborema.add_edge(city, year_of_analysis(cases_paraiba), weight=weight_of_edge_city)
 
+    # Cria o grafo do estado da Paraíba a partir dos subgrafos das regiões
     graph_pb = nx.Graph()
     graph_pb.add_nodes_from(YEARS)
-    graph_pb.add_nodes_from(borborema.nodes)
-    graph_pb.add_nodes_from(sertao.nodes)
-    graph_pb.add_nodes_from(agreste.nodes)
-    graph_pb.add_nodes_from(mata.nodes)
-    graph_pb.add_edges_from(borborema.edges)
-    graph_pb.add_edges_from(agreste.edges)
-    graph_pb.add_edges_from(sertao.edges)
-    graph_pb.add_edges_from(mata.edges)
 
-    # Configuração de tamanho de vértice e cor - grafo geral
+    for gr in [agreste, borborema, mata, sertao]:
+        graph_pb.add_nodes_from(gr.nodes())
+        graph_pb.add_edges_from(gr.edges())
+
+    # Obtendo de tamanho dos vértices do grafo da Paraíba a partir da centralidade de autovetor
     eigenvector_centrality_graph_geral = nx.eigenvector_centrality(graph_pb)
     eigenvector_centrality_graph_geral_list = list(eigenvector_centrality_graph_geral.items())
     eigenvector_centrality_graph_geral_list.sort(key=lambda x: x[1], reverse=True)
 
+    # Obtendo o tamanho dos vértices dos grafos das regiões a partir da centralidade de grau
+    degree_centrality_agreste = nx.degree_centrality(agreste)
+    degree_centrality_borborema = nx.degree_centrality(borborema)
+    degree_centrality_mata = nx.degree_centrality(mata)
+    degree_centrality_sertao = nx.degree_centrality(sertao)
 
-    degree_centrality_graph_borborema = nx.degree_centrality(borborema)
-    degree_centrality_graph_sertao = nx.degree_centrality(sertao)
-    degree_centrality_graph_agreste = nx.degree_centrality(agreste)
-    degree_centrality_graph_mata = nx.degree_centrality(mata)
+    # Cores e tamanhos do grafo e subgrafos
+    graphs_attributes = {
+        "paraiba": {"size": [], "color": []},
+        "agreste": {"size": [], "color": []},
+        "borborema": {"size": [], "color": []},
+        "mata": {"size": [], "color": []},
+        "sertao": {"size": [], "color": []},
+    }
 
-    color_nodes_geral = list()
-    size_nodes_geral = list()
+    region_map = {
+        "sertao": (sertao, COR_SERTAO),
+        "agreste": (agreste, COR_AGRESTE),
+        "borborema": (borborema, COR_BORBOREMA),
+        "mata": (mata, COR_MATA),
+    }
 
-    size_nodes_borborema = list()
-    size_nodes_sertao = list()
-    size_nodes_agreste = list()
-    size_nodes_mata = list()
-    color_nodes_borborema = list()
-    color_nodes_sertao = list()
-    color_nodes_agreste = list()
-    color_nodes_mata = list()
-
-    for n in graph_pb.nodes:
-        if n in YEARS:
-            color_nodes_geral.append("#FF0000")
-            color_nodes_borborema.append("#FF0000")
-            color_nodes_sertao.append("#FF0000")
-            color_nodes_agreste.append("#FF0000")
-            color_nodes_mata.append("#FF0000")
-            size_nodes_geral.append(1000 + (15000 * eigenvector_centrality_graph_geral[n]))
-        elif n in sertao.nodes:
-            color_nodes_geral.append("#FFD700")
-            color_nodes_sertao.append("#FFD700")
-            size_nodes_geral.append(1000 +  (10000 * eigenvector_centrality_graph_geral[n]))
-        elif n in agreste.nodes:
-            color_nodes_geral.append("#BAD94D")
-            color_nodes_agreste.append("#BAD94D")
-            size_nodes_geral.append(1000 + (10000 * eigenvector_centrality_graph_geral[n]))
-        elif n in borborema.nodes:
-            color_nodes_geral.append("#EDA621")
-            color_nodes_borborema.append("#EDA621")
-            size_nodes_geral.append(1000 + (10000 * eigenvector_centrality_graph_geral[n]))
-        elif n in mata.nodes:
-            color_nodes_geral.append("#08CF2C")
-            color_nodes_mata.append("#08CF2C")
-            size_nodes_geral.append(1000 + (10000 * eigenvector_centrality_graph_geral[n]))
+    for node in graph_pb.nodes:
+        if node in YEARS:
+            color = COR_ANO
+            size = CONSTANTE_SOMA + (CONSTANTE_MULT * eigenvector_centrality_graph_geral[node])
+            for region in graphs_attributes.keys():
+                graphs_attributes[region]["color"].append(color)
+            graphs_attributes["paraiba"]["size"].append(size)
+        else:
+            for region, (graph, color) in region_map.items():
+                if node in graph.nodes:
+                    size = CONSTANTE_SOMA + (CONSTANTE_MULT * eigenvector_centrality_graph_geral[node])
+                    graphs_attributes["paraiba"]["color"].append(color)
+                    graphs_attributes["paraiba"]["size"].append(size)
+                    graphs_attributes[region]["color"].append(color)
+                    break
 
     # Plotagem Grafo Geral
-    left = YEARS
-    fig, bloco = plt.subplots(figsize=(60, 60))  # Define o tamanho total da figura
-    fig.subplots_adjust(left=0, right=1, top=1, bottom=0)  # Remove as margens
-    nx.draw_networkx(graph_pb, pos=nx.spring_layout(graph_pb), node_size=size_nodes_geral,
-                     node_color=color_nodes_geral, font_weight='bold')
-    plt.show()
+    plot_geral_graph(graph_pb, graphs_attributes["paraiba"]["size"], graphs_attributes["paraiba"]["color"])
 
     # Ranking com top 5 cidades e 5 anos mais importantes através de centralidade de autovetor
     rank_cities = ranking(eigenvector_centrality_graph_geral_list, False)
     rank_years = ranking(eigenvector_centrality_graph_geral_list)
 
-    print(f'Top 5 Cidades com mais Casos ao longo dos Anos')
-    for c in rank_cities:
-        print(c)
-    print()
-    print(f'Top 5 Anos com mais Casos ao longo dos Anos')
-    for a in rank_years:
-        print(a)
-    print()
+    labels_rank_autovetor = []
+    values_rank_autovetor = []
+    for label, value in rank_years:
+        labels_rank_autovetor.append(label)
+        values_rank_autovetor.append(round(value, 3))
 
+    bar_chart(values_rank_autovetor, labels_rank_autovetor, COR_ANO, "Ano de Notificação", "Centralidade de Autovetor", "Gráfico de Barras CA")
+    show_ranking(rank_cities, rank_years)
 
-    # Centralidade de proximidade para identificar anos que não ocorreram a doença
-    c_p_g_pb = nx.closeness_centrality(graph_pb)
-    if not absance_of_cases_in_year(c_p_g_pb):
-        print('Houve casos em todos os anos')
-    else:
-        print('No ano: ', absance_of_cases_in_year(c_p_g_pb), "não houve casos")
+    # Centralidade de proximidade para identificar anos que não ocorreram a doença em regiões
 
+    for name, (graph_region, color) in region_map.items():
+        closeness_centrality_region = nx.closeness_centrality(graph_region)
+        labels_closeness = []
+        value_closeness = []
 
-    a1 = nx.degree_centrality(agreste)
-    b1 = nx.degree_centrality(borborema)
-    m1 = nx.degree_centrality(mata)
-    s1 = nx.degree_centrality(sertao)
+        for years_region in YEARS:
+            labels_closeness.append(years_region)
+            value_closeness.append(round(closeness_centrality_region[years_region], 2))
 
-    a1_size = list()
-    b1_size = list()
-    m1_size = list()
-    s1_size = list()
+        if not absance_of_cases_in_year(closeness_centrality_region):
+            print(f'A região {name.title()} apresentou casos em todos os anos')
 
-    a1_color = list()
-    b1_color = list()
-    m1_color = list()
-    s1_color = list()
+        else:
+            print(f'A região {name.title()} não apresentou casos no(s) ano(s): ', absance_of_cases_in_year(closeness_centrality_region))
+            bar_chart(value_closeness, labels_closeness, color, 'Anos', 'Centralidade de Proximidade',
+                      f"Gráfico de Barras CP - {name}")
 
-    # Plotagem subregioes
-    config_graph_agreste = plot_subgraph(agreste, a1, "#FF0000", "#BAD94D")
-    a1_size = config_graph_agreste[1]
-    a1_color = config_graph_agreste[0]
-    plot_graph(agreste, a1_color, a1_size, 'Agreste')
+    # Tupla com infos dos subgrafos
+    subgraphs = [
+        (agreste, degree_centrality_agreste, COR_AGRESTE, 'Agreste'),
+        (borborema, degree_centrality_borborema, COR_BORBOREMA, 'Borborema'),
+        (mata, degree_centrality_mata, COR_MATA, 'Zona da Mata'),
+        (sertao, degree_centrality_sertao, COR_SERTAO, 'Sertão'),
+    ]
 
-    config_graph_borborema = plot_subgraph(borborema, b1, "#FF0000", "#EDA621")
-    b1_size = config_graph_borborema[1]
-    b1_color = config_graph_borborema[0]
-    plot_graph(borborema, b1_color, b1_size, 'Borborema')
+    cardinality_pb = cardinality(graph_pb) - len(YEARS)
+    percentage_regions = []
 
+    # Plotagem subgrafo e infos gráfico de setores
+    for subgraph_pb, centrality, color, name in subgraphs:
+        color_list_subgraph, size_list_subgraph = config_plot_subgraph(subgraph_pb, centrality, COR_ANO, color)
+        plot_graph(subgraph_pb, color_list_subgraph, size_list_subgraph, name)
+
+        percentage_region = ((cardinality(subgraph_pb) - len(YEARS)) * 100) / cardinality_pb
+        percentage_regions.append(round(percentage_region, 2))
+
+    # Plot grafico de setores
+    pie_chart(percentage_regions, ['Agreste', 'Borborema', 'Zona da Mata', 'Sertão'],
+              [COR_AGRESTE, COR_BORBOREMA, COR_MATA, COR_SERTAO])
--- utils.py
+++ utils.py
@@ -5,52 +5,155 @@ import json
 import os
 import chardet
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
+from matplotlib.pyplot import title
+from networkx.algorithms.centrality import eigenvector_centrality
+from networkx.drawing import bipartite_layout
 
 # Constante com as variáveis representadas em cada coluna
-ANALYSIS_VARIABLES = ['DT_NOTIFIC', 'NU_ANO', 'ID_MUNICIP', 'SG_UF_NOT',
-                      'ANO_NASC', 'CS_SEXO', 'CS_GESTANT', 'CLASSI_FIN',
-                      'EVOLUCAO', 'DT_OBITO']
+ANALYSIS_VARIABLES = ['DT_NOTIFIC', 'NU_ANO', 'ID_MUNICIP', 'SG_UF_NOT']
 
 YEARS = ['2015', '2016','2017', '2018', '2019', '2020', '2021', '2022', '2023', '2024']
 
+# Diretórios necessários
+DIR_CITIES = "rsc/cities.json"
+MESORREGIOES = 'rsc/mesoregioes.json'
+FIG_GRAFO_PARAIBA = "figures/Paraiba.png"
+DIR_UFCODIG = "data/processed/UFccodig.cnv"
+DIR_MUNICIP = "data/processed/Municpb.cnv"
+DIR_DATA_PROCESSED = "data/processed/LEIVBR"
+
+# Regiões e estado
+SERTAO = 'SERTAO'
+BORBOREMA = 'BORBOREMA'
+AGRESTE = 'AGRESTE'
+MATA = 'MATA'
+ESTADO = 'PB'
+
+# Cores vertices
+COR_ANO = "#5F9EA0"
+COR_AGRESTE = "#BAD94D"
+COR_MATA = "#08CF2C"
+COR_BORBOREMA = "#EDA621"
+COR_SERTAO = "#FFD700"
+
+CONSTANTE_SOMA = 3000
+CONSTANTE_MULT = 50000
+MUNICIPIO_ID = 'ID_MUNICIP'
+
+def bar_chart(values: list, labels_bar: list, color: str, x: str, y: str, name: str) -> None:
+
+    if len(labels_bar) != len(values):
+        raise "Erro de tamanho entre as listas"
+    plt.figure(figsize=[7,7])
+    plt.bar(labels_bar, values, color=color)
+    plt.xlabel(x, font='Times New Roman', fontsize=14)
+    plt.ylabel(y, font='Times New Roman', fontsize=14)
+    plt.savefig(f"figures/{name}.png")
+    plt.show()
+
+
+def pie_chart(percentage: list, labels_pie: list, list_colors: list) -> None:
+
+    plt.figure(figsize = (7, 7))
+    plt.plot(labels_pie, percentage)
+    plt.pie(percentage, labels = labels_pie, autopct='%1.0f%%', colors = list_colors)
+    plt.savefig(f"figures/Gráfico de Setores Regiões Notificadas.png")
+    plt.show()
+
+
+def cardinality(grafo: nx.Graph) -> int:
+    """
+    :param grafo: grafo de análise
+    :return: um inteiro correspondendo a cardinalidade/quantidade de vértices do grafo
+    """
+    return len(grafo.nodes())
+
+def show_ranking(rank_cities: list, rank_years: list) -> None:
+    """
+    :param rank_cities:
+    :param rank_years:
+    :return:
+    """
+    print(f'Top 5 Cidades com mais Casos ao longo dos Anos')
+    for c in rank_cities:
+        print(c)
+
+    print()
+    print(f'Top 5 Anos com mais Casos ao longo dos Anos')
+    for a in rank_years:
+        print(a)
+    print()
+
+    # (Grafico de barras)
 
 # Percorre o dataframe e conta quantas vezes a cidade apareceu
 def weight_of_city(city_or_code: str, database: pd.DataFrame) -> int:
-    cont = database['ID_MUNICIP'].value_counts().get(int(city_or_code), 0)
+    """
+    Função que retorna a quantidade de casos (peso da aresta) de um municipio
+    :param city_or_code: codigo da cidade
+    :param database: dataframe para pesquisa
+    :return:
+    """
+    cont = database[MUNICIPIO_ID].value_counts().get(int(city_or_code), 0)
     return cont
 
 
-def plot_subgraph(grafo_regiao: nx.Graph, dic_size: dict, color1: str, color2: str):
+def config_plot_subgraph(grafo_regiao: nx.Graph, dic_size: dict, color1: str, color2: str):
     """
+    Configuração de plotagem dos subgrafos - cores e tamanhos
     :param grafo_regiao: grafo da regiao
     :param dic_size: dicionario com as centralidade de grau para definição do tamanho
     :param color1: cor para os vértices de ano
     :param color2: cor para os vértices de cidade
-    :return: uma lista com uma dois listas, uma para as cores e outra para os tamanhos
+    :return: uma lista com uma duas listas, uma para as cores e outra para os tamanhos
     """
-    lc = list()
-    ls = list()
+    color_list = list()
+    size_list = list()
     for n in grafo_regiao.nodes:
-        lc.append(color1) if n in YEARS else lc.append(color2)
-        ls.append(1000 + (dic_size[n] * 3500))
+        color_list.append(color1) if n in YEARS else color_list.append(color2)
+        size_list.append(500 + (dic_size[n] * 7500))
 
-    return [lc, ls]
+    return [color_list, size_list]
 
+
+def plot_geral_graph(grafo: nx.Graph, list_size_nodes: list, list_color_nodes: list) -> None:
+    fig, bloco = plt.subplots(figsize=(60, 60))  # Define o tamanho total da figura
+    fig.subplots_adjust(left=0, right=1, top=1, bottom=0)  # Remove as margens
+    nx.draw_networkx(grafo, pos=nx.spring_layout(grafo, scale=100), node_size=list_size_nodes,
+                     node_color=list_color_nodes, font_weight='bold', font_size=20)
+
+    plt.savefig(FIG_GRAFO_PARAIBA, format='png', dpi=150)
+    plt.show()
 
 
 def plot_graph(grafo: nx.Graph, color: list, size: list, regiao: str) -> None:
+    """
+    Função para plotagem de grafo das regiões
+    :param grafo: grafo para plotagem
+    :param color: lista de cores
+    :param size: lista de tamanhos
+    :param regiao: nome da região que será apresentado
+    :return: não retorna nada, apenas plota o grafo
+    """
 
-    fig, bloco = plt.subplots(figsize=(35, 35))
-    fig.subplots_adjust(left=0, right=1, top=0.90, bottom=0)
+    fig, bloco = plt.subplots(figsize=(28, 28))
+    fig.subplots_adjust(left=0, right=1, top=1, bottom=0)
     nx.draw_networkx(grafo, with_labels=True, pos=nx.bipartite_layout(grafo, YEARS), node_color=color,
-                     font_weight='bold', node_size=size)
-    bloco.set_title(f'Casos de Leishmaniose na Paraíba nos anos 2015-2019 - Região - {regiao}', fontsize=45)
+                     font_weight='bold', node_size=size, font_size=20)
+    #bloco.set_title(f'Casos de Leishmaniose na Paraíba nos anos 2015-2019 - Região - {regiao}', fontsize=45)
     edge_labels = nx.get_edge_attributes(grafo, 'weight')
-    nx.draw_networkx_edge_labels(grafo, pos=nx.bipartite_layout(grafo, YEARS), edge_labels=edge_labels, font_size=12,
-                                 font_weight='bold', horizontalalignment='right', ax=bloco)
+    nx.draw_networkx_edge_labels(grafo, pos=nx.bipartite_layout(grafo, YEARS), edge_labels=edge_labels, font_size=15,
+                                  font_weight='bold', horizontalalignment='right', ax=bloco, bbox={"alpha": 0}, verticalalignment='center')
+    plt.savefig(f"figures/{regiao}.png", format='png', dpi=150)
     plt.show()
 
 def ranking(list_cities_and_years: list, years=True) -> list:
+    """
+    :param list_cities_and_years: lista de tuplas com centralidade
+    :param years: True se é uma lista de anos ou False se não é
+    :return: retorna uma lista com o rank das cinco cidades ou anos
+    """
 
     classifieds_cities = list()
     classifieds_years = list()
@@ -75,9 +178,14 @@ def absance_of_cases_in_year(dictionary_with_closeness_centrality: dict):
     :return: retorna o ano em que não houve casos ou False caso tenha ocorrido casos na região
     """
 
+    anos_ausentes_sem_casos_notificados = list()
+
     for k, v in dictionary_with_closeness_centrality.items():
         if k in YEARS and v == 0.0:
-            return k
+            anos_ausentes_sem_casos_notificados.append(k)
+
+    if anos_ausentes_sem_casos_notificados:
+        return anos_ausentes_sem_casos_notificados
     return False
 
 def detected_encoding(arquivo):
@@ -167,7 +275,7 @@ def create_a_dataframe(state: str, file_path: str) -> pd.DataFrame:
     """
 
     # Obtém o código do estado em formato de inteiro
-    state = get_code(state, "src/states.json")
+    state = get_code(state, "rsc/states.json")
     state = int(state)
 
     # Verifica se todas as colunas necessárias estão presentes
@@ -181,6 +289,7 @@ def create_a_dataframe(state: str, file_path: str) -> pd.DataFrame:
     formatted_data = unformatted_data.copy(deep=True)
     # Seleciona somente as linhas em que a coluna "SG_UF_NOT" são do código referente ao estado escolhido
     formatted_data = formatted_data.loc[formatted_data['SG_UF_NOT'] == state]
+
     return formatted_data.fillna('X')
 
 def year_of_analysis(data: pd.DataFrame) -> str:
@@ -203,7 +312,7 @@ def convert_cnv_to_json(file: str, file_name_save: str, name_code=True) -> True:
     """
 
     # Diretorios importantes
-    BASE_DIR = os.path.abspath("src")
+    BASE_DIR = os.path.abspath("rsc")
     SAVE_TO = os.path.join(BASE_DIR, (file_name_save+".json"))
 
     # Dicionario que com cidade/estado que será transformado em json
@@ -213,37 +322,29 @@ def convert_cnv_to_json(file: str, file_name_save: str, name_code=True) -> True:
         with open(file, 'r+', encoding=detected_encoding(file)) as f:
             with open(file, 'r+', encoding=detected_encoding(file)) as f:
                 lines = f.readlines()
-
                 # Lê as linhas e faz o ajuste no nome
                 for l in range(len(lines)):
                     lines[l] = lines[l].split()
-
                     # Verifica o tamanho da linha
                     if len(lines[l]) < 3 or ';' in lines[l]:
                         continue
-
                     # Ajusta o último elemento (código dos dados desconhecidos)
                     elif ',' in lines[l][-1]:
                         lines[l][-1] = lines[l][-1].replace(',', '-')
                         lines[l][-1] = lines[l][-1].strip('-')
-
                     # Formata um municipio
                     elif (lines[l][-1] == lines[l][1]) or ('-' in lines[l][-1] and '9999' in lines[l][-1]):
                         lines[l] = treats_city(lines[l])
-
                     # Formata um estado
                     elif (len(lines[l]) >= 3 and len(lines[l][-1]) == 2) or ('-' in lines[l][-1] and '00' in lines[l][-1]):
                         lines[l] = treats_state(lines[l])
-
                     # Criação de chaves e valores para cidades ou estados
                     info_local = lines[l][0].upper()                           # Nome
                     code_local = lines[l][1]                                   # Código
-
                     if name_code:
                         save_local[info_local] = code_local
                     else:
                         save_local[code_local] = info_local
-
             with open(SAVE_TO, 'w+', encoding="utf8") as a:
                 json.dump(save_local, a, indent=4, ensure_ascii=False)
             return True
